2a2b15b374999e47d2842d460018ff1d7a70c6ca
park204683 <park204683@gmail.com>
2026-07-08 12:29:41 +0900

    3차 업로드

---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.header("이곳이 디폴트 홈입니다.")
+
+st.title("강남구청")
+st.write("Hello World!")
+st.header("이건 무슨")
+st.markdown("# haha\n ```python a=10```\n**abcd**\n*efgh*")
---
+++ page_1 copy.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from dotenv import load_dotenv
+from openai import OpenAI
+import re
+
+load_dotenv()
+
+st.title("My ChatBot")
+if "messages" not in st.session_state :
+    st.session_state.messages=[]
+client = OpenAI()
+
+
+prompt = st.chat_input("무엇이든 물어보십시오.")
+if prompt:
+    response = client.responses.create(
+    model="gpt-5.5",
+    input=prompt
+    )
+    print(response.output_text)
+    st.session_state.messages.append({'role':'user','content':prompt})
+    st.session_state.messages.append({'role':'ai','content':{response.output_text}})
+    
+    for message in st.session_state.messages :
+        with st.chat_message(message['role']) : 
+            clean_text =re.sub(r"[\{|\}]","",message["content"]) 
+            st.write(f"{message["content"]}")
